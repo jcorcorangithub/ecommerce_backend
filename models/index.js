dfe17638af849c99e5845267e2a6b7394a9fa72f
James Corcoran <jpcsoccer1234@gmail.com>
2021-05-10 20:47:29 -0400

    began writing out association methods on index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,34 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  //
+});
 
-// Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+
+Category.hasMany(Product, {
+  //
+});
+
+
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+});
+
+
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+});
 
 module.exports = {
   Product,
